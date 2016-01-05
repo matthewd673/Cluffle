437249144902eea147d25fc5204b39f65364d6e7
Matthew Daly <matthewd673@gmail.com>
2016-01-05 10:10:15 -0500

    revert proxy

--- proxy.php
+++
@@ -1,2 +0,0 @@
-<h1>Sadly, the proxy is gone</h1>
-<p>When Cluffle was shifted over to Heroku, I couldn't migrate the proxy as well without a huge hassle. Maybe it will be back someday to grace us with its proxyness once more</p>
